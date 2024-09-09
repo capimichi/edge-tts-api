2508bdbbfdbde52a46f679d00bb7e3cbd175fe32
Michele Capichioni <capimichi@gmail.com>
2024-09-09 22:59:59 +0200

    Added FastAPI implementation for Edge TTS API including text-to-speech endpoint and voice list endpoint.

---
+++ api.py
@@ -0,0 +1,45 @@
+import os
+import tempfile
+from typing import List
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import edge_tts
+import uvicorn
+
+from edgettsapi.model.TtsRequest import TtsRequest
+from edgettsapi.model.Voice import Voice
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+base_url = os.getenv("EDGE_TTS_BASE_URL", "http://localhost:8000")
+
+app = FastAPI(title="Edge TTS API", version="0.1.0", root_path=base_url)
+
+@app.post("/tts")
+async def text_to_speech(tts_request: TtsRequest) -> FileResponse:
+    text = tts_request.get_text()
+    voice_name = tts_request.get_voice()
+    communicate = edge_tts.Communicate(text, voice_name)
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
+    temp_file_path = temp_file.name
+    await communicate.save(temp_file_path)
+    # audio_file = open(temp_file_path, "rb")
+    # audio_file_content = audio_file.read()
+    return FileResponse(temp_file_path, media_type="audio/mpeg")
+
+@app.get("/voices")
+async def get_voices() -> List[Voice]:
+    voices_items = await edge_tts.list_voices()
+    voices = []
+    for voice in voices_items:
+        if voice:
+            voices.append(Voice.model_validate(voice))
+
+    return voices
+
+
+if(__name__ == "__main__"):
+    uvicorn.run(app, host="0.0.0.0", port=8000)
