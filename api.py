58ca50551c5e2dd857db80dcd7917f0c5cc601e9
Michele Capichioni <capimichi@gmail.com>
2024-11-13 22:16:24 +0100

    Use EDGE_TTS_PORT environment variable for server port in main execution

--- api.py
+++ api.py
@@ -42,4 +42,4 @@ async def get_voices() -> List[Voice]:
 
 if(__name__ == "__main__"):
     port = int(os.getenv("EDGE_TTS_PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
